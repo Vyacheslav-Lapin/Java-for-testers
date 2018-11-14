920faeaae1e3f7e844466d2a3618e48b9b2b4d5c
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2018-11-14 15:47:26 +0500

    todo - Slot

---
+++ src/main/java/casino/Slot.java
@@ -0,0 +1,31 @@
+package casino;
+
+public final class Slot {
+
+  public static void main(String... __) {
+
+    int money; // TODO: 2018-11-14 считать кол-во денег в виде числа
+
+    // (true) { // TODO: 2018-11-14 проверить на совпадение
+      // TODO: 2018-11-14 выдать выигрыш в случае победы
+    { // TODO: 2018-11-14 цикл пока не закончатся деньги
+      double random = Math.random();
+      int usilie = (int) Math.round(random * 100);
+
+      int[] baraban1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+      int[] baraban2 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+      int[] baraban3 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+      // TODO: 2018-11-14 провернуть барабаны (все 3) и определить каждую ячейку
+      int baraban1Position = 0;
+      int baraban2Position = 0;
+      int baraban3Position = 0;
+
+      // TODO: 2018-11-14 уменьшить или увеличить money и выйти
+    }
+
+    if (true) { // TODO: 2018-11-14 проверить на наличие денег
+      // TODO: 2018-11-14 выдать выигрыш в случае победы
+    }
+  }
+}
