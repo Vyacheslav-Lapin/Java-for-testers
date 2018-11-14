127763b297af5b5da8a62fbb5dde9811a3b0500a
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2018-11-14 16:52:00 +0500

    Slot - done! (first variant)

--- src/main/java/casino/Slot.java
+++ src/main/java/casino/Slot.java
@@ -2,30 +2,35 @@ package casino;
 
 public final class Slot {
 
-  public static void main(String... __) {
+  private final static int BARABAN_LENGTH = 7;
 
-    int money; // TODO: 2018-11-14 считать кол-во денег в виде числа
+  public static void main(String[] args) {
 
-    // (true) { // TODO: 2018-11-14 проверить на совпадение
-      // TODO: 2018-11-14 выдать выигрыш в случае победы
-    { // TODO: 2018-11-14 цикл пока не закончатся деньги
-      double random = Math.random();
-      int usilie = (int) Math.round(random * 100);
+    int money = Integer.parseInt(args[0]);
 
-      int[] baraban1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-      int[] baraban2 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-      int[] baraban3 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    while (money > 0) {
 
-      // TODO: 2018-11-14 провернуть барабаны (все 3) и определить каждую ячейку
-      int baraban1Position = 0;
-      int baraban2Position = 0;
-      int baraban3Position = 0;
+      money -= 10;
 
-      // TODO: 2018-11-14 уменьшить или увеличить money и выйти
+      int usilie1 = (int) Math.round(Math.random() * 100);
+      int usilie2 = (int) Math.round(Math.random() * 100);
+      int usilie3 = (int) Math.round(Math.random() * 100);
+
+      int baraban1Position = usilie1 % BARABAN_LENGTH;
+      int baraban2Position = usilie2 % BARABAN_LENGTH;
+      int baraban3Position = usilie3 % BARABAN_LENGTH;
+
+      if (baraban1Position == baraban2Position
+          && baraban1Position == baraban3Position) {
+        money += 10_000;
+        break;
+      }
     }
 
-    if (true) { // TODO: 2018-11-14 проверить на наличие денег
-      // TODO: 2018-11-14 выдать выигрыш в случае победы
+    if (money > 0) {
+      System.out.println("Вы выиграли! Забирайте свои деньги: $" + money);
+    } else {
+      System.out.println("Вы проиграли! Соболезнуем...");
     }
   }
 }
